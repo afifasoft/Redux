ce2c1916afe092c3b832d6006c222207fecc5016
afifasoft <abdul.pesit@gmail.com>
2019-04-21 23:11:34 +0400

    Break up reducer into multiple reducer

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -13,7 +13,7 @@ var stateDefault = {
 var nextHobbyId = 1;
 var nextMoiveId = 1;
 
-var reducer = (state = stateDefault, action) => {
+var oldReducer = (state = stateDefault, action) => {
 //  state = state || {name : 'Anonymous'};
 
 
@@ -66,6 +66,62 @@ var reducer = (state = stateDefault, action) => {
   }
 
 };
+
+var nameReducer = (state = 'Anonymous', action) => {
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return action.name;
+
+    default:
+      return state;
+
+  }
+};
+
+var hobbiesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_HOBBY':
+      return [
+        ...state,
+        {
+          id: nextHobbyId++,
+          hobby: action.hobby
+        }
+      ];
+
+    case 'REMOVE_HOBBY':
+      return state.filter((hobby) => hobby.id !== action.id)
+
+    default:
+      return state;
+  }
+};
+
+var moviesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_MOVIE':
+      return [
+        ...state,
+        {
+          id: nextMoiveId++,
+          title: action.title,
+          genre: action.genre,
+        }
+      ];
+
+    case 'REMOVE_MOVIE':
+      return state.filter((movie) => movie.id !== action.id)
+
+    default:
+      return state;
+  }
+};
+
+var reducer = redux.combineReducers({
+    name : nameReducer,
+    hobbies: hobbiesReducer,
+    movies: moviesReducer
+})
 
 var store = redux.createStore(reducer, redux.compose(
   window.devToolsExtension ? window.devToolsExtension() : f => f
