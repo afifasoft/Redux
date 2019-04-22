c73ca19b35b2d6184b8c9545f280528f567caa30
afifasoft <abdul.pesit@gmail.com>
2019-04-22 23:56:34 +0400

    Add support for location fetching

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,5 @@
 var redux = require('redux');
+var axios = require('axios');
 
 console.log('Starting redux example');
 
@@ -161,10 +162,54 @@ var removeMovie = (id) => {
     }
 };
 
+// Map reducer and action generators
+// --------------------
+var mapReducer = (state = {isFetching: false, url: undefined}, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+
+    default:
+      return state;
+  }
+};
+
+var startLocationFetch = () => {
+    return {
+      type: 'START_LOCATION_FETCH'
+    }
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  }
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then(function (res) {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q=';
+
+    store.dispatch(completeLocationFetch(baseUrl + loc))
+  });
+};
 var reducer = redux.combineReducers({
     name : nameReducer,
     hobbies: hobbiesReducer,
-    movies: moviesReducer
+    movies: moviesReducer,
+    map: mapReducer
 })
 
 var store = redux.createStore(reducer, redux.compose(
@@ -175,15 +220,23 @@ var store = redux.createStore(reducer, redux.compose(
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
+
   console.log('Name is', state.name);
-  document.getElementById('app').innerHTML = state.name;
+  //document.getElementById('app').innerHTML = state.name;
 
   console.log('New State:', store.getState());
+
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="'+ state.map.url +'" target="_blank">View Your Location</a>'
+  }
 });
 
 var currentState = store.getState();
-
 console.log('currentState', currentState);
+
+fetchLocation();
 
 store.dispatch(changeName('Atif'));
 
